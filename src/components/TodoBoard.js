497ac52ffbc5d476fa9a216e2231fabd9cdc8a76
wntjdus <tjdusdl0811@naver.com>
2025-02-04 19:27:33 +0900

    proxy setting

--- src/components/TodoBoard.js
+++ src/components/TodoBoard.js
@@ -5,8 +5,7 @@ const TodoBoard = ({todoList, toggleComplete, deleteItem}) => {
   return (
     <div>
       <h2>Todo List</h2>
-      {todoList.length > 0 ? todoList.map((item)=> <TodoItem item={item} toggleComplete={toggleComplete} 
-      deleteItem={deleteItem}/>): <h2>There is no Item to show</h2>}
+      {todoList.length > 0 ? todoList.map((item)=> <TodoItem item={item} toggleComplete={toggleComplete} deleteItem={deleteItem}/>): <h2>There is no Item to show</h2>}
       {/* <TodoItem/> will be here once we get the todoList */}
       
     </div>
